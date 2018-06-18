61f31d4c965c635ce2b85aaff3bbefc84aa8db28
pdwarkanath <dwarkanath.prabhu@gmail.com>
2018-06-18 03:57:18 -0500

    ex5 completed

--- week6/machine-learning-ex5/ex5/validationCurve.m
+++ week6/machine-learning-ex5/ex5/validationCurve.m
@@ -38,8 +38,12 @@ error_val = zeros(length(lambda_vec), 1);
 %       end
 %
 %
-
-
+for i = 1:length(lambda_vec)
+    lambda = lambda_vec(i);
+    theta = trainLinearReg(X, y, lambda);
+    [error_train(i), dum1] = linearRegCostFunction(X, y, theta, 0);
+    [error_val(i),dum2] = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 
 
